ccd6dc2a7d416f766e5197f19d15c17eabfd13c6
Rishabh <95174850+Rishabh-Codes27@users.noreply.github.com>
2024-01-18 12:05:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ import { Cart } from './Components/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <ProductsPage/>
-    </div>
+    // <div className="App">
+    //   <ProductsPage/>
+    // </div>
+
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className='text-white text-xl'>Products</Link>
+            </li>
+
+            <li>
+              <Link to="/cart" className='text-white text-xl'>Cart</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path="/" element={<ProductsPage/>}></Route>
+          <Route exact path="/cart" element={<Cart/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/ProductsPage.jsx
+++ src/Components/ProductsPage.jsx
@@ -1,58 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import ProductData from './actions/ProductData'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+// import ProductData from './actions/ProductData'
+import axios from "axios";
 
 export const ProductsPage = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   function getProducts() {
     axios
       .get("https://fakestoreapi.com/products")
       .then((res) => {
-        if(res.status === 200) {
-          console.log(res.data)
-          setProducts(res.data)
+        if (res.status === 200) {
+          console.log(res.data);
+          setProducts(res.data);
         }
       })
       .catch((err) => {
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
-  window.onload = function() {
-    getProducts()
-  }
+  window.onload = function () {
+    getProducts();
+  };
 
   return (
-    <div  onLoad={getProducts}>
-      <p className='text-white text-xl'>Products:</p>
+    <div onLoad={getProducts}>
+      <p className="text-white text-xl">Products:</p>
       {products && (
-      <div>
-        {products.map((item, i) => {
-          return(
-            <div>
+        <div>
+          {products.map((item, i) => {
+            return (
               <div>
-                <p>
-                  {""}
-                  {item.title}{" "}
-                </p>
+                <div>
+                  <p>
+                    {""}
+                    {item.title}{" "}
+                  </p>
 
-                <p>
-                  {" "}
-                  {item.description} {" "}
-                </p>
+                  <p> {item.description} </p>
+                </div>
 
-                <p>
-                  $ {item.price}
-                </p>
+                <div>
+                  <p>$ {item.price}</p>
 
-                <img src={item.image} width={250} height={250} alt="" />
+                  <img src={item.image} width={250} height={250} alt="" />
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>
+            );
+          })}
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
